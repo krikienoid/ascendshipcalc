9d89f9a73f80c406db5a90c962b3654afaab6e3a
Ken Sugiura <ken.sugiura@gmail.com>
2015-12-04 20:28:45 -0500

    converted tabs to spaces

--- src/js/ascend-ship-img.js
+++ src/js/ascend-ship-img.js
@@ -8,81 +8,81 @@
 
 ;(function (window, $, undefined) {
 
-	'use strict';
+    'use strict';
 
-	//
-	// Data
-	//
+    //
+    // Data
+    //
 
-	var componentSpriteWidth  = 74,
-		componentSpriteHeight = 56,
-		componentSpritePos;
+    var componentSpriteWidth  = 74,
+        componentSpriteHeight = 56,
+        componentSpritePos;
 
-	//
-	// Sprites
-	//
+    //
+    // Sprites
+    //
 
-	function createComponentSprite (i, isLabeled) {
-		var pos = componentSpritePos[i],
-			cx  = -componentSpriteWidth  * pos.x,
-			cy  = -componentSpriteHeight * pos.y,
-			$sprite = $('<div/>')
-				.addClass('component-display')
-				.append(
-					$('<div/>')
-						.addClass('component-sprite')
-						.css({'backgroundPosition' : cx + 'px ' + cy + 'px'})
-				);
-		if (isLabeled) {
-			$sprite
-				.addClass('labeled')
-				.prepend(
-					$('<div/>')
-						.addClass('component-name')
-						.append(
-							$('<small/>').text(ascend.components[i].name)
-						)
-				);
-		}
-		return $sprite;
-	}
+    function createComponentSprite (i, isLabeled) {
+        var pos = componentSpritePos[i],
+            cx  = -componentSpriteWidth  * pos.x,
+            cy  = -componentSpriteHeight * pos.y,
+            $sprite = $('<div/>')
+                .addClass('component-display')
+                .append(
+                    $('<div/>')
+                        .addClass('component-sprite')
+                        .css({'backgroundPosition' : cx + 'px ' + cy + 'px'})
+                );
+        if (isLabeled) {
+            $sprite
+                .addClass('labeled')
+                .prepend(
+                    $('<div/>')
+                        .addClass('component-name')
+                        .append(
+                            $('<small/>').text(ascend.components[i].name)
+                        )
+                );
+        }
+        return $sprite;
+    }
 
-	//
-	// Init
-	//
+    //
+    // Init
+    //
 
-	;(function initComponentSpritePos () {
+    ;(function initComponentSpritePos () {
 
-		componentSpritePos = [];
+        componentSpritePos = [];
 
-		// No Device
-		componentSpritePos.push({x : 11, y : 6});
+        // No Device
+        componentSpritePos.push({x : 11, y : 6});
 
-		// Weapons
-		for (var i = 0; i < 10; i++) componentSpritePos.push({x : i, y : 0});
+        // Weapons
+        for (var i = 0; i < 10; i++) componentSpritePos.push({x : i, y : 0});
 
-		// Shields and Engines
-		for (var i = 0; i < 11; i++) componentSpritePos.push({x : i, y : 1});
+        // Shields and Engines
+        for (var i = 0; i < 11; i++) componentSpritePos.push({x : i, y : 1});
 
-		// Scanners and Generators
-		for (var i = 0; i < 11; i++) componentSpritePos.push({x : i, y : 2});
+        // Scanners and Generators
+        for (var i = 0; i < 11; i++) componentSpritePos.push({x : i, y : 2});
 
-		// Specials
-		for (var i = 0; i < 44; i++) {
-			componentSpritePos.push({
-				x : i % 12,
-				y : 3 + Math.floor(i / 12)
-			});
-		}
+        // Specials
+        for (var i = 0; i < 44; i++) {
+            componentSpritePos.push({
+                x : i % 12,
+                y : 3 + Math.floor(i / 12)
+            });
+        }
 
-	})();
+    })();
 
-	//
-	// Export
-	//
+    //
+    // Export
+    //
 
-	if (!window.ascend) {window.ascend = {};}
+    if (!window.ascend) {window.ascend = {};}
 
-	window.ascend.createComponentSprite = createComponentSprite;
+    window.ascend.createComponentSprite = createComponentSprite;
 
 })(window, jQuery); //\/\/
